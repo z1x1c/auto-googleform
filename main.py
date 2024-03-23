aaecaf4ca66d4be2777b5e8287a6220592c74dff
z1x1c <>
2024-03-23 21:34:33 +1300

    Refactor timezone conversion function and add new functions

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import pytz
 # Settings
 url = ""
 timezone = "Pacific/Auckland"
-timestamps = {}
+timestamps = ""
 
 # Retrive link at specific date and time
-def get_timezone_adjusted_timestamp(timezone, timestamp) -> datetime:
-    tz = pytz.timezone(timezone)
-    tz_now = datetime.now(tz)
+def get_timezone_adjusted_timestamp(timezone_str, timestamp_str) -> datetime:
+    try:
+        timestamp = datetime.fromisoformat(timestamp_str)
+        target_timezone = pytz.timezone(timezone_str)
+        tz_aware_timestamp = timestamp.replace(tzinfo=pytz.utc).astimezone(target_timezone)
 
-    return tz_now
+        return tz_aware_timestamp
+    except Exception as e:
+        print(f"Error converting timestamp to local time ({timezone})")
 
 # Form autofill script
 def fill_form():
@@ -20,9 +24,23 @@ def fill_form():
 def submit_form():
     pass
 
+def is_time():
+    pass
+
+def form_is_open():
+    pass
+
+def run():
+    print("Starting...")
+    while not(is_time()):
+        print("")
+    while not(form_is_open()):
+        print("Checking if form open...")
+    fill_form()
+    submit_form()
+
 def main():
     print("Running script", flush = True)
-    submit_form()
 
 if __name__ == "__main__":
     main()
