8995cf28ed8b6a8e389dd329bb828600954a06db
z1x1c <zqpm4500@gmail.com>
2024-03-27 12:25:36 +1300

    Update form submission with email and payment info

--- main.py
+++ main.py
@@ -33,11 +33,12 @@ def form_is_open(url):
         raise e
 
 def fill_form():
-    email = "test@test.com"
+    email = "python@email.com"
     first_name = "John"
     last_name = "Doe"
     player_level = "Intermediate"
     is_member = "Yes"
+    will_pay = "Yes"
     
     value = {
             "entry.emailAddress": email,
@@ -45,29 +46,41 @@ def fill_form():
             "entry.1846655546": last_name,
             "entry.877086558": player_level,
             "entry.1459019614": is_member,
+            "entry.861646923": will_pay,
+            "pageHistory": "0,1",
             }
+    
+    return value
 
 
-def submit_form():
-    pass
+def submit_form(url, data):
+    user_agent = {'Referer':'none','User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
+
+    session = requests.Session()
+
+    try:
+        res = session.post(url, data = data)
+        if res.status_code != 200:
+            raise Exception(f"Error {res.status_code}")
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        raise e
 
 def run():
-    url = ""
+    url = input("Enter form url: ").replace("viewform", "formResponse")
 
     print("Starting...")
 
-    while not(is_time()) and not():
-        sleep(3)
-        print("Not time yet...", flush = True)
-    while not(form_is_open(url)):
-        sleep(3)
-        print("Checking if form open...", flush = True)
+    # while not(is_time()) and not():
+    #     sleep(3)
+    #     print("Not time yet...", flush = True)
+    # while not(form_is_open(url)):
+    #     sleep(3)
+    #     print("Checking if form open...", flush = True)
 
-    fill_form()
-    submit_form()
-
-def main():
-    run()
+    form_data = fill_form()
+    submit_form(url, form_data)
 
 if __name__ == "__main__":
-    main()
+    run()
